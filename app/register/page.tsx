dbe69f15018dd42a37f1f7256598c86561806d4c
yadi-soe <yadisoe122734@gmail.com>
2026-03-27 16:32:58 +0200

    add auth ui

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,9 +2,10 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { defaultLoginRoutePath } from "@/lib/constants";
 
 
-export default function Home() {
+export default function Register() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-600 font-sans dark:bg-black py-20">
       <main className="flex min-h-screen w-full max-w-4xl flex-col items-center px-16 bg-gray-600 dark:bg-black">
@@ -20,10 +21,10 @@ export default function Home() {
           <CardHeader>
             <CardTitle
               className="text-4xl text-white"
-              >Get Started Now!</CardTitle>
+              >Register Now!</CardTitle>
             <CardDescription 
               className="text-xl">
-              Please login to your account to continue
+              Enter your details below to Sign Up
             </CardDescription>
           </CardHeader>
           <CardContent className="w-full mt-5">
@@ -39,6 +40,19 @@ export default function Home() {
                   className="w-full border bg-zinc-800 border-none text-gray-400 px-5 py-4 text-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 />
               </div>
+
+               <div className="flex flex-col gap-3 mt-5">
+                <label htmlFor="email" className="text-xl text-white font-medium">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  className="w-full border bg-zinc-800 border-none text-gray-400 px-5 py-4 text-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+
               <div className="flex flex-col gap-3 mt-5">
                 <label htmlFor="password" className="text-xl text-white font-medium">
                   Password
@@ -51,6 +65,18 @@ export default function Home() {
                 />
               </div>
 
+               <div className="flex flex-col gap-3 mt-5">
+                <label htmlFor="confirmPassword" className="text-xl text-white font-medium">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  placeholder="Confirm your password"
+                  className="w-full border bg-zinc-800 border-none text-gray-400 px-5 py-4 text-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+
               <div className="flex items-center justify-between gap-2 py-5">
                 <div className="flex items-center gap-3">
                   <input
@@ -59,26 +85,21 @@ export default function Home() {
                       className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500 focus:ring-offset-0 focus:ring-offset-transparent focus:ring-2 focus:outline-none"
                     />
                   <Label htmlFor="remember" className="text-xl text-gray-400">
-                    Remember me
+                    I have read and agree to the Terms and Conditions
                   </Label>
-                </div>
-                <div className="">
-                  <Link href="/register/forgetPassword" className="text-xl underline text-gray-400">
-                    Forgot password?
-                  </Link>
                 </div>
               </div>
 
               <button
                 type="submit"
                 className="w-full font-bold bg-[#EC6C00] px-3 py-5 text-black text-2xl hover:bg-[#D45A00] focus:outline-none focus:ring-2 focus:ring-[#EC6C00] focus:ring-offset-2">
-                Login
+                Sign Up
               </button>
 
               <div className="flex items-center justify-center mt-5">
-                <p className="text-xl text-white">Have an account?</p>
-                <Link href="/register" className="ml-5 text-xl underline text-white">
-                  Sign Up
+                <p className="text-xl text-white">Already have an account?</p>
+                <Link href={defaultLoginRoutePath} className="ml-5 text-xl underline text-white">
+                  Login
                 </Link>
               </div>
             </form>
@@ -89,3 +110,4 @@ export default function Home() {
     </div>
   );
 }
+
